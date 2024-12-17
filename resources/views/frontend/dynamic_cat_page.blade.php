7cbbebe6691ab247fe0c3718cfc93dc964ecd027
Keshav <kj6560@gmail.com>
2024-12-17 12:24:29 +0530

    minor change

--- resources/views/frontend/dynamic_cat_page.blade.php
+++ resources/views/frontend/dynamic_cat_page.blade.php
@@ -20,165 +20,37 @@
 <div class="page-content">
 
     <!-- Portfolio Grid col 4 -->
-		<section class="section-md">
-			<div class="container-fluid px-4">
-				<div class="row pbmit-element-posts-wrapper">
-					<article class="pbmit-ele-portfolio pbmit-portfolio-style-2 col-md-6 col-lg-3">
-						<div class="pbminfotech-post-content">
-							<div class="pbmit-featured-img-wrapper">
-								<div class="pbmit-featured-wrapper">
-									<img src="{{asset('brahmani_frontend_assets')}}/images/portfolio/portfolio-01b.jpg" class="img-fluid" alt="">
-								</div>
-							</div>
-							<div class="pbminfotech-box-content">
-								<div class="pbminfotech-titlebox">
-									<div class="pbmit-port-cat">
-										<a href="portfolio-grid-col-3.html" rel="tag">Bedroom</a>
-									</div>
-									<h3 class="pbmit-portfolio-title">
-										<a href="portfolio-detail-style-1.html">Innovation</a>
-									</h3>
-								</div>
-							</div>
-						</div>
-					</article>
-					<article class="pbmit-ele-portfolio pbmit-portfolio-style-2 col-md-6 col-lg-3">
-						<div class="pbminfotech-post-content">
-							<div class="pbmit-featured-img-wrapper">
-								<div class="pbmit-featured-wrapper">
-									<img src="{{asset('brahmani_frontend_assets')}}/images/portfolio/portfolio-02b.jpg" class="img-fluid" alt="">
-								</div>
-							</div>
-							<div class="pbminfotech-box-content">
-								<div class="pbminfotech-titlebox">
-									<div class="pbmit-port-cat">
-										<a href="portfolio-grid-col-3.html" rel="tag">Furniture</a>
-									</div>
-									<h3 class="pbmit-portfolio-title">
-										<a href="portfolio-detail-style-1.html">Minimalism</a>
-									</h3>
-								</div>
-							</div>
-						</div>
-					</article>
-					<article class="pbmit-ele-portfolio pbmit-portfolio-style-2 col-md-6 col-lg-3">
-						<div class="pbminfotech-post-content">
-							<div class="pbmit-featured-img-wrapper">
-								<div class="pbmit-featured-wrapper">
-									<img src="{{asset('brahmani_frontend_assets')}}/images/portfolio/portfolio-03b.jpg" class="img-fluid" alt="">
-								</div>
-							</div>
-							<div class="pbminfotech-box-content">
-								<div class="pbminfotech-titlebox">
-									<div class="pbmit-port-cat">
-										<a href="portfolio-grid-col-3.html" rel="tag">Interior</a>
-									</div>
-									<h3 class="pbmit-portfolio-title">
-										<a href="portfolio-detail-style-1.html">Lighting</a>
-									</h3>
-								</div>
-							</div>
-						</div>
-					</article>
-					<article class="pbmit-ele-portfolio pbmit-portfolio-style-2 col-md-6 col-lg-3">
-						<div class="pbminfotech-post-content">
-							<div class="pbmit-featured-img-wrapper">
-								<div class="pbmit-featured-wrapper">
-									<img src="{{asset('brahmani_frontend_assets')}}/images/portfolio/portfolio-04b.jpg" class="img-fluid" alt="">
-								</div>
-							</div>
-							<div class="pbminfotech-box-content">
-								<div class="pbminfotech-titlebox">
-									<div class="pbmit-port-cat">
-										<a href="portfolio-grid-col-3.html" rel="tag">Kitchen</a>
-									</div>
-									<h3 class="pbmit-portfolio-title">
-										<a href="portfolio-detail-style-1.html">Bold Tiles</a>
-									</h3>
-								</div>
-							</div>
-						</div>
-					</article>
-					<article class="pbmit-ele-portfolio pbmit-portfolio-style-2 col-md-6 col-lg-3">
-						<div class="pbminfotech-post-content">
-							<div class="pbmit-featured-img-wrapper">
-								<div class="pbmit-featured-wrapper">
-									<img src="{{asset('brahmani_frontend_assets')}}/images/portfolio/portfolio-05b.jpg" class="img-fluid" alt="">
-								</div>
-							</div>
-							<div class="pbminfotech-box-content">
-								<div class="pbminfotech-titlebox">
-									<div class="pbmit-port-cat">
-										<a href="portfolio-grid-col-3.html" rel="tag">Bedroom</a>
-									</div>
-									<h3 class="pbmit-portfolio-title">
-										<a href="portfolio-detail-style-1.html">Clean lines</a>
-									</h3>
-								</div>
-							</div>
-						</div>
-					</article>
-					<article class="pbmit-ele-portfolio pbmit-portfolio-style-2 col-md-6 col-lg-3">
-						<div class="pbminfotech-post-content">
-							<div class="pbmit-featured-img-wrapper">
-								<div class="pbmit-featured-wrapper">
-									<img src="{{asset('brahmani_frontend_assets')}}/images/portfolio/portfolio-06b.jpg" class="img-fluid" alt="">
-								</div>
-							</div>
-							<div class="pbminfotech-box-content">
-								<div class="pbminfotech-titlebox">
-									<div class="pbmit-port-cat">
-										<a href="portfolio-grid-col-3.html" rel="tag">Architecture</a>
-									</div>
-									<h3 class="pbmit-portfolio-title">
-										<a href="portfolio-detail-style-1.html">Integral</a>
-									</h3>
-								</div>
-							</div>
-						</div>
-					</article>
-					<article class="pbmit-ele-portfolio pbmit-portfolio-style-2 col-md-6 col-lg-3">
-						<div class="pbminfotech-post-content">
-							<div class="pbmit-featured-img-wrapper">
-								<div class="pbmit-featured-wrapper">
-									<img src="{{asset('brahmani_frontend_assets')}}/images/portfolio/portfolio-07b.jpg" class="img-fluid" alt="">
-								</div>
-							</div>
-							<div class="pbminfotech-box-content">
-								<div class="pbminfotech-titlebox">
-									<div class="pbmit-port-cat">
-										<a href="portfolio-grid-col-3.html" rel="tag">Interior</a>
-									</div>
-									<h3 class="pbmit-portfolio-title">
-										<a href="portfolio-detail-style-1.html">Functionality</a>
-									</h3>
-								</div>
-							</div>
-						</div>
-					</article>
-					<article class="pbmit-ele-portfolio pbmit-portfolio-style-2 col-md-6 col-lg-3">
-						<div class="pbminfotech-post-content">
-							<div class="pbmit-featured-img-wrapper">
-								<div class="pbmit-featured-wrapper">
-									<img src="{{asset('brahmani_frontend_assets')}}/images/portfolio/portfolio-08b.jpg" class="img-fluid" alt="">
-								</div>
-							</div>
-							<div class="pbminfotech-box-content">
-								<div class="pbminfotech-titlebox">
-									<div class="pbmit-port-cat">
-										<a href="portfolio-grid-col-3.html" rel="tag">Furniture</a>
-									</div>
-									<h3 class="pbmit-portfolio-title">
-										<a href="portfolio-detail-style-1.html">Terracotta</a>
-									</h3>
-								</div>
-							</div>
-						</div>
-					</article>
-				</div>
-			</div>
-		</section>
-		<!-- Portfolio Grid col 4 End -->
+    <section class="section-md">
+        <div class="container-fluid px-4">
+            <div class="row pbmit-element-posts-wrapper">
+                @foreach ($category_products as $category_product)
+                    <article class="pbmit-ele-portfolio pbmit-portfolio-style-2 col-md-6 col-lg-3">
+                        <div class="pbminfotech-post-content">
+                            <div class="pbmit-featured-img-wrapper">
+                                <div class="pbmit-featured-wrapper">
+                                    <img src="{{asset('brahmani_frontend_assets')}}/images/portfolio/portfolio-01b.jpg"
+                                        class="img-fluid" alt="">
+                                </div>
+                            </div>
+                            <div class="pbminfotech-box-content">
+                                <div class="pbminfotech-titlebox">
+                                    <div class="pbmit-port-cat">
+                                        <a href="portfolio-grid-col-3.html" rel="tag">Bedroom</a>
+                                    </div>
+                                    <h3 class="pbmit-portfolio-title">
+                                        <a href="portfolio-detail-style-1.html">Innovation</a>
+                                    </h3>
+                                </div>
+                            </div>
+                        </div>
+                    </article>
+                @endforeach
+
+
+            </div>
+        </div>
+    </section>
+    <!-- Portfolio Grid col 4 End -->
 
 </div>
 <!-- Page Content End -->
