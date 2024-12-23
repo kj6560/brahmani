12d4ec6abf88e1220ae7f248091391b6613d1089
Keshav <kj6560@gmail.com>
2024-12-23 17:51:24 +0530

    abcd

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -184,8 +184,7 @@
         <!-- partial:../../partials/_footer.html -->
         <footer class="footer">
           <div class="d-sm-flex justify-content-center justify-content-sm-between">
-            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block"><a href="#"
-                target="_blank">{{$settings['Company_Name']??""}}</a> </span>
+            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">{{$settings['Company_Name']??""}} </span>
             <span class="float-none float-sm-end d-block mt-1 mt-sm-0 text-center">Copyright © {{date('Y')}}. All rights
               reserved.</span>
           </div>
